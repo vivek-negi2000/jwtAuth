854823a747333b612f92edcf97daa15a2678cc0c
Vivek-Negi-30077991 <vivek.negi@adani.com>
2022-09-06 12:24:56 +0530

    Added Class

--- JwtTokenAuth/Controllers/CustomerController.cs
+++ JwtTokenAuth/Controllers/CustomerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using JwtTokenAuth.Data;
+using JwtTokenAuth.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +11,47 @@ namespace JwtTokenAuth.Controllers
     
     public class CustomerController : ControllerBase
     {
+        private readonly LoginContext _db;
+
+        public CustomerController(LoginContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet, Authorize]
         public IEnumerable<string>  Get()
         {
             return new string[]  { "john","Doe","naveen"};
         }
+
+        [HttpGet("Logged-Users"), Authorize]
+        public IEnumerable<LoginModel> Details()
+        {
+            return _db.logins.ToList();
+        }
+
+        [HttpGet("GetLoginModelData")]
+        public LoginModel GetLoginModelData()
+        {
+            try
+            {
+                // Model object creation.
+                LoginModel objLoginModel = new LoginModel();
+
+                // data filling into the object.
+                objLoginModel.UserName = "Vivek sir";
+                objLoginModel.Password = "123456";
+
+                //data return
+                return objLoginModel;
+            }
+            catch(Exception)
+            {
+                throw ;
+            }
+           
+        }
     }
+
+    
 }
