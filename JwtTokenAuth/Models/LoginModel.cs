f799e70fee60bbd17200105eaf37a303aff83cf0
vivek negi <vivek negi@LAPTOP-N4ACP3SL>
2023-04-02 18:45:23 +0530

    this is 1st commit

--- JwtTokenAuth/Models/LoginModel.cs
+++ JwtTokenAuth/Models/LoginModel.cs
@@ -7,5 +7,6 @@ namespace JwtTokenAuth.Models
         [Key]
         public string? UserName { get; set; }
         public string? Password { get; set; }
+//added this comment for gitPractice
     }
 }
